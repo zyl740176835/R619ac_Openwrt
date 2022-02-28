c1b5b289142e83af17049d70e29be290b59dc5b2
zyl740176835 <61375483+zyl740176835@users.noreply.github.com>
2022-03-01 01:20:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,8 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 find  feeds/luci/applications/ -name luci-app-easymesh -exec rm -rf {} \;
 git clone https://github.com/zyl740176835/luci-app-easymesh feeds/luci/applications/luci-app-easymesh
+git clone https://github.com/zyl740176835/luci-app-smartdns package/luci-app-smartdns
+git clone https://github.com/honwen/luci-app-aliddns package/luci-app-aliddns
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 
