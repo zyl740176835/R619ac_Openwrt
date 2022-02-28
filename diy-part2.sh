245d929ac7ae6063bca8f8def138e48c70090fb2
zyl740176835 <61375483+zyl740176835@users.noreply.github.com>
2022-02-28 23:04:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 # Modify default IP  
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
-find ./  -name smartdns -exec rm -rf {} \; && find ./  -name luci-app-easymesh -exec rm -rf {} \;
-git clone https://github.com/zyl740176835/luci-app-easymesh package/luci-app-easymesh;
-git clone https://github.com/zyl740176835/smartdns package/smartdns;
-git clone https://github.com/zyl740176835/luci-app-smartdns package/luci-app-smartdns
+find  feeds/luci/applications/ -name luci-app-easymesh -exec rm -rf {} \;
+git clone https://github.com/zyl740176835/luci-app-easymesh feeds/luci/applications/luci-app-easymesh
+
+
